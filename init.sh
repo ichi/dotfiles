fdf06dab1d16c98967b640f7d70a748c2e1e782a
ichi <ichi.ttht.1@gmail.com>
2011-07-06 15:37:16 +0900

    ln -sf するスクリプト

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+ln -sf ./.colordiffrc ~/
+ln -sf ./.emacs ~/
+ln -sf ./.emacs.d ~/
+ln -sf ./.gitconfig ~/
+ln -sf ./.gitignore_global ~/
+ln -sf ./.hgrc ~/
+ln -sf ./.hgignore_global ~/
+ln -sf ./.zshrc ~/
