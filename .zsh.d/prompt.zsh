b2ddae073f7ecf8ee6c29d6082f1b7723d25d0ba
ichi <ichi.ttht.1@gmail.com>
2025-04-09 19:54:08 +0900

    vscodeでRPROMPT非表示

--- .zsh.d/prompt.zsh
+++ .zsh.d/prompt.zsh
@@ -18,13 +18,16 @@ esac
 PROMPT="%F{red}[%*] %n${PROMPT_MARK}%f"
 PROMPT2="%F{red}%_%%%f"
 
-# RPROMPT="%{$bg[magenta]$fg[white]%}[ %~ ]%{$reset_color%}"
-# RPROMPT="%K{magenta}%F{white}[ %~ ]%k%f"
+# VSCodeのターミナルでは、psvarを使うとエージェントが出力結果をとってこれないので
+if [[ "$TERM_PROGRAM" != "vscode" ]]; then
+    # RPROMPT="%{$bg[magenta]$fg[white]%}[ %~ ]%{$reset_color%}"
+    RPROMPT="%K{magenta}%F{white}[ %~ ]%k%f"
 
-# RPROMPT="%1(v|%{$fg[green]%}(%1v%3(v|%{$fg[red]%}:%3v|)%2(v|%{$fg[yellow]%}⚡|)%{$fg[green]%})%{$reset_color%}|)%{$bg[magenta]$fg[white]%}[ %~ ]%{$reset_color%}"
-RPROMPT="%1(v|%{$fg[green]%}(%1v%3(v|%{$fg[red]%}:%3v|)%2(v|%{$fg[yellow]%}⚡|)%{$fg[green]%})%{$reset_color%}|)%{$bg[magenta]$fg[white]%}[ %~ ]%{$reset_color%}"
+    # RPROMPT="%1(v|%{$fg[green]%}(%1v%3(v|%{$fg[red]%}:%3v|)%2(v|%{$fg[yellow]%}⚡|)%{$fg[green]%})%{$reset_color%}|)%{$bg[magenta]$fg[white]%}[ %~ ]%{$reset_color%}"
+    RPROMPT="%1(v|%F{green}(%1v%3(v|%F{red}:%3v|)%2(v|%F{yellow}⚡|)%F{green})%f|)${RPROMPT}"
 
-RPROMPT2="${RPROMPT}"
+    RPROMPT2="${RPROMPT}"
+fi
 
 # SPROMPT="%{$fg[red]%}%r is correct? [n,y,a,e]:%{$reset_color%} "
 SPROMPT="%F{red}%r is correct? [n,y,a,e]:%f"
@@ -48,7 +51,8 @@ esac
 
 # remote host
 if [ -n "${REMOTEHOST}${SSH_CONNECTION}" ]; then
-    PROMPT="%{$fg[white]%}${HOST%%.*}%{$reset_color%} ${PROMPT}"
+    # PROMPT="%{$fg[white]%}${HOST%%.*}%{$reset_color%} ${PROMPT}"
+    PROMPT="%F{white}${HOST%%.*}%f ${PROMPT}"
 fi
 
 # add space
@@ -61,3 +65,15 @@ function _chpwd_custom_title() {
     printf '\e]1;%s\a' `basename $PWD`
 }
 add-zsh-hook chpwd _chpwd_custom_title
+
+# コマンド実行後改行
+function _break_line_after_command() {
+    # Print a newline before the prompt, unless it's the
+    # first prompt in the process.
+    if [ -z "$NEW_LINE_BEFORE_PROMPT" ]; then
+        NEW_LINE_BEFORE_PROMPT=1
+    elif [ "$NEW_LINE_BEFORE_PROMPT" -eq 1 ]; then
+        echo ""
+    fi
+}
+add-zsh-hook precmd _break_line_after_command
